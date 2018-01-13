107af50b85ed2c93a90ab400c8c5db4fb0a30110
Danilo Piparo <danilo.piparo@cern.ch>
2018-01-13 01:44:16 +0100

    Extend example

--- test/intro.C
+++ test/intro.C
@@ -1,11 +1,20 @@
-#include "../inc/ROOT/TVec.hxx"
 
+#ifdef __CLING__
+#include "../inc/ROOT/TVec.hxx"
+#else
+#include "ROOT/TVec.hxx"
+#endif
+
+#include "TRandom.h"
+#include <ROOT/TDataFrame.hxx>
+
+using namespace ROOT::Experimental;
 using namespace ROOT::Experimental::VecOps;
 
 template <typename T>
 void Print(const TVec<T> &v)
 {
-   std::cout << v << std::endl;
+   std::cout << cling::printValue((TVec<T> *)&v) << std::endl;
 }
 
 void intro()
@@ -55,6 +64,10 @@ void intro()
       auto res0 = v00 + v01;
       Print(res0);
 
+      std::cout << "Multiplication by TVec ({4,5,6,7}):" << std::endl;
+      auto res00 = v00 * v01;
+      Print(res00);
+
       std::cout << "Division by TVec ({4,5,6,7}):" << std::endl;
       TVec<int> v10{0, 1, 2, 3};
       auto res1 = v10 / v01;
@@ -80,10 +93,49 @@ void intro()
       std::cout << "v0 = " << v0 << std::endl;
       std::cout << "v1 = " << v1 << std::endl;
       std::cout << "v2 = " << v2 << std::endl;
-      std::cout << "v0 + 1 = " << v0+1 << std::endl;
-      std::cout << "v1 - v2 = " << v1-v2 << std::endl;
-      std::cout << "(v1 - v2) / 3. = " << (v1-v2)/3. << std::endl;
-      std::cout << "v0 + 1 + (v1 - v2) / 3. = " << v0 + 1 + (v1-v2)/3. << std::endl;
-      //std::cout << "v0 + 1 + (v1 - v2) / 3. > 3 = " << v0 + 1 + (v1-v2)/3. > 3 << std::endl;
+      std::cout << "v0 + 1 = " << v0 + 1 << std::endl;
+      std::cout << "v1 - v2 = " << v1 - v2 << std::endl;
+      std::cout << "(v1 - v2) / 3. = " << (v1 - v2) / 3. << std::endl;
+      std::cout << "v0 + 1 + (v1 - v2) / 3. = " << v0 + 1 + (v1 - v2) / 3. << std::endl;
+      // Cannot print directly... To be debugged!
+      auto res = (v0 + 1 + (v1 - v2) / 3.) > 3;
+      std::cout << "(v0 + 1 + (v1 - v2) / 3.) > 3  " << cling::printValue((TVec<int> *)&res) << std::endl;
+   }
+
+   return;
+   // The example below still has some problems
+
+   {
+      std::cout << "Now using a TTreeReaderArray to initialise, mimic what would happen in a TDF" << std::endl;
+      // Ideally: tdf.Define("pt", "sqrt(px*px + py*py)").Histo1D("pt"); or, now not possible in TDF,
+      // tdf.Histo1D("sqrt(px*px + py*py)")
+
+      if (false){
+         auto rndmVector = []() {
+            std::vector<double> v((unsigned)gRandom->Uniform(16));
+            for (auto &&e : v) {
+               e = gRandom->Gaus();
+            }
+            return v;
+         };
+         TDataFrame tdf(8);
+         tdf.Define("px", rndmVector).Define("py", rndmVector).Snapshot("t", "dataset.root");
+      }
+      auto f = TFile::Open("dataset.root");
+      TTreeReader myReader("t", f);
+      TTreeReaderArray<double> px(myReader, "px");
+      TTreeReaderArray<double> py(myReader, "py");
+
+      // So far so good. Now the serious stuff
+      TH1F h("myhisto", "The Histo", 64, 0, 2);
+      while (myReader.Next()) {
+         const TVec<double> pxv(px);
+         const TVec<double> pyv(py);
+         TVec<float> v(px.GetSize());
+         const auto ptv = pxv*v;//sqrt(pxv*pxv + pyv*pyv);
+         std::cout << ptv[0] << std::endl;
+      }
    }
 }
+
+int main() {intro();return 0;}
