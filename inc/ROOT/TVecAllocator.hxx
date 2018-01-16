7e12ff53d66fe382f3413c8f9dc0cf21d895fd02
Danilo Piparo <danilo.piparo@cern.ch>
2018-01-16 15:31:22 +0100

    Add the TVecAllocator and its unit tests
    this allocator works like the stl::allocator but the user
    can set an inital pointer and size which will be returned at the
    first invocation.

---
+++ inc/ROOT/TVecAllocator.hxx
@@ -0,0 +1,72 @@
+// Author: Enrico Guiraud, Enric Tejedor, Danilo Piparo CERN  01/2018
+
+/*************************************************************************
+ * Copyright (C) 1995-2018, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#ifndef ROOT_TVECALLOCATOR
+#define ROOT_TVECALLOCATOR
+
+#include <iostream>
+
+#include <memory>
+
+namespace ROOT {
+namespace Detail {
+namespace VecOps {
+
+template<typename T>
+class TVecAllocator // : public std::allocator<T>
+{
+public:
+   using StdAlloc_t = std::allocator<T>;
+   using value_type = typename StdAlloc_t::value_type;
+   using pointer = typename StdAlloc_t::pointer;
+   using const_pointer = typename StdAlloc_t::const_pointer;
+   using reference = typename StdAlloc_t::reference;
+   using const_reference = typename StdAlloc_t::const_reference;
+   using size_type = typename StdAlloc_t::size_type;
+   using difference_type = typename StdAlloc_t::difference_type;
+
+private:
+
+   using StdAllocTraits_t = std::allocator_traits<StdAlloc_t>;
+   StdAlloc_t fStdAllocator;
+   bool fIsFirstAllocation = true;
+   pointer fInitialAddress = nullptr;
+   size_type fInitialSize = 0;
+
+public:
+void SetInitialMemory( size_type n, pointer addr)
+{
+   fInitialSize = n;
+   fInitialAddress = addr;
+}
+
+pointer allocate( std::size_t n )
+{
+   if(n > std::size_t(-1) / sizeof(T)) throw std::bad_alloc();
+   if (fIsFirstAllocation) {
+      fIsFirstAllocation = false;
+      return fInitialAddress;
+   }
+   return StdAllocTraits_t::allocate(fStdAllocator, n);
+}
+
+void deallocate( pointer p, std::size_t n )
+{
+   if (p != fInitialAddress) StdAllocTraits_t::deallocate(fStdAllocator, p, n);
+}
+
+
+};
+
+} // End NS VecOps
+} // End NS Internal
+} // End NS ROOT
+
+#endif
---
+++ test/tvecallocator_unit.cxx
@@ -0,0 +1,40 @@
+#include <ROOT/TVecAllocator.hxx>
+
+#include <gtest/gtest.h>
+
+#include <vector>
+#include <iostream>
+using namespace ROOT::Detail::VecOps;
+
+TEST(TVecAllocator, ReusePointer)
+{
+   std::vector<double> vmodel {1,2,3};
+   TVecAllocator<double> alloc0;
+   alloc0.SetInitialMemory(vmodel.size(), vmodel.data());
+
+   EXPECT_EQ(vmodel.data(), alloc0.allocate(123));
+
+   TVecAllocator<double> alloc1;
+   alloc1.SetInitialMemory(vmodel.size(), vmodel.data());
+
+   std::vector<double, TVecAllocator<double>> v(vmodel.size(), double(), alloc1);
+
+   EXPECT_EQ(vmodel.data(), v.data());
+
+   for (auto i : {0, 1, 2}) {
+      EXPECT_EQ(vmodel[i], v.at(i));
+   }
+
+   v.emplace_back(3.);
+   EXPECT_NE(vmodel.data(), v.data());
+
+   auto res = 0;
+   try {
+      v.resize(-1);
+   } catch (...)
+   {
+      res = 1;
+   }
+   EXPECT_EQ(1, res);
+
+}
